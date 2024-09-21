3db18ce48a31a98de91ee7e23f965c4e2e3dc2d2
Ibad18 <ibad1818@gmail.com>
2024-09-21 13:13:44 +0500

    Assignment completed

--- server.mjs
+++ server.mjs
@@ -3,19 +3,15 @@ const PORT = 3000;
 const myServer = createServer((req, res) => {
     if (req.url === '/'|| req.url === '/home') {
         res.writeHead(200, { 'content-type': 'text/plain' })
-        res.end("Home Page")
+        res.end("This is Home Page")
     }
     else if (req.url === '/about') {
         res.writeHead(200, { 'content-type': 'text/plain' })
-        res.end("About Page")
+        res.end("This is About Page")
     }
     else if (req.url === '/contact') {
         res.writeHead(200, { 'content-type': 'text/plain' })
-        res.end("Contact Page")
-    }
-    else if (req.url === '/section') {
-        res.writeHead(200, { 'content-type': 'text/plain' })
-        res.end("Section Page")
+        res.end("This is a Contact Page")
     }
     else {
         res.writeHead(404, { 'content-type': 'text/plain' })
